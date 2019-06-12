dc1ba3117ed85f419ca2ba8b0c43889aada78f10
zongf0504 <15737250504@139.com>
2019-06-12 11:23:08 +0800

    调整断言工具类方法顺序. 按是否需要自定义异常描述进行归类.

--- utils-common/src/main/java/org/zongf/utils/common/util/AssertUtil.java
+++ utils-common/src/main/java/org/zongf/utils/common/util/AssertUtil.java
@@ -27,20 +27,6 @@ public class AssertUtil {
     }
 
     /**
-     * @Description: 判断对象非空
-     * @param object 要判断的对象, 可为任意类型
-     * @param exDesc 异常描述信息
-     * @since 1.0
-     * @author: zongf
-     * @time: 2019-06-12 10:19:18
-     */
-    public static void notNull(Object object, String exDesc) {
-        if (object == null) {
-            throw new AssertException(exDesc);
-        }
-    }
-
-    /**
      * @Description: 校验字符串非空, null和一组空格均视为空
      * @param str 字符串
      * @since 1.0
@@ -54,6 +40,62 @@ public class AssertUtil {
     }
 
     /**
+     * @Description: 校验集合非空, null和0个元素均视为空
+     * @param collection 集合对象, 可为List, Set, Collection等
+     * @since 1.0
+     * @author: zongf
+     * @time: 2019-06-12 10:19:18
+     */
+    public static void notEmpty(Collection collection) {
+        if (collection == null || collection.size() == 0) {
+            throw new AssertException("集合为空!");
+        }
+    }
+
+    /**
+     * @Description: 校验Map非空, null和0个元素均视为空
+     * @param map map对象
+     * @since 1.0
+     * @author: zongf
+     * @time: 2019-06-12 10:19:18
+     */
+    public static void notEmpty(Map map) {
+        if (map == null || map.size() == 0) {
+            throw new AssertException("集合为空!");
+        }
+    }
+
+    /**
+     * @Description: 校验数组非空, null和0个元素均视为空
+     * @param array 任意类型数组,非int[],long[]等基本类型数组
+     * @since 1.0
+     * @author: zongf
+     * @time: 2019-06-12 10:19:18
+     */
+    public static void notEmpty(Object[] array) {
+        if (array == null || array.length == 0) {
+            throw new AssertException("集合为空!");
+        }
+    }
+
+    //************************************** 自定义异常信息 *****************************************
+
+    /**
+     * @Description: 判断对象非空
+     * @param object 要判断的对象, 可为任意类型
+     * @param exDesc 异常描述信息
+     * @since 1.0
+     * @author: zongf
+     * @time: 2019-06-12 10:19:18
+     */
+    public static void notNull(Object object, String exDesc) {
+        if (object == null) {
+            throw new AssertException(exDesc);
+        }
+    }
+
+
+    /**
      * @Description: 校验字符串非空, null和一组空格均视为空
      * @param str 字符串
      * @param exDesc 异常描述信息
@@ -64,19 +106,6 @@ public class AssertUtil {
     public static void notEmpty(String str, String exDesc) {
         if (str == null || "".equals(str.trim())) {
             throw new AssertException(exDesc);
-        }
-    }
-
-    /**
-     * @Description: 校验集合非空, null和0个元素均视为空
-     * @param collection 集合对象, 可为List, Set, Collection等
-     * @since 1.0
-     * @author: zongf
-     * @time: 2019-06-12 10:19:18
-     */
-    public static void notEmpty(Collection collection) {
-        if (collection == null || collection.size() == 0) {
-            throw new AssertException("集合为空!");
         }
     }
 
@@ -97,19 +126,6 @@ public class AssertUtil {
     /**
      * @Description: 校验Map非空, null和0个元素均视为空
      * @param map map对象
-     * @since 1.0
-     * @author: zongf
-     * @time: 2019-06-12 10:19:18
-     */
-    public static void notEmpty(Map map) {
-        if (map == null || map.size() == 0) {
-            throw new AssertException("集合为空!");
-        }
-    }
-
-    /**
-     * @Description: 校验Map非空, null和0个元素均视为空
-     * @param map map对象
      * @param exDesc 异常描述信息
      * @since 1.0
      * @author: zongf
@@ -121,5 +137,18 @@ public class AssertUtil {
         }
     }
 
+    /**
+     * @Description: 校验数组非空, null和0个元素均视为空
+     * @param array 任意类型数组,非int[],long[]等基本类型数组
+     * @param exDesc 异常描述信息
+     * @since 1.0
+     * @author: zongf
+     * @time: 2019-06-12 10:19:18
+     */
+    public static void notEmpty(Object[] array, String exDesc) {
+        if (array == null || array.length == 0) {
+            throw new AssertException(exDesc);
+        }
+    }
 
 }
