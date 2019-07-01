bfdcf4756bc2a6a0f99faad2b4b636fab817feb5
zongf0504 <15737250504@139.com>
2019-07-01 10:48:15 +0800

    修改注释格式

--- utils-common/src/main/java/org/zongf/utils/common/util/ValidateUtil.java
+++ utils-common/src/main/java/org/zongf/utils/common/util/ValidateUtil.java
@@ -3,33 +3,30 @@ package org.zongf.utils.common.util;
 import java.util.Collection;
 import java.util.Map;
 
-/**
- * @Description: 验证工具类
+/**验证工具类
  * @since 1.0
- * @author: zongf
- * @date: 2019-06-12 10:39
+ * @author zongf
+ * @created 2019-07-01
  */
 public class ValidateUtil {
 
-    /**
-     * @Description: 判断对象是否为null
+    /**判断对象是否为null
      * @param object 任意类型对象
-     * @return: true/false
+     * @return 对象为null 返回true, 否则返回false
      * @since 1.0
-     * @author: zongf
-     * @time: 2019-06-12 10:42:05
+     * @author zongf
+     * @created 2019-07-01
      */
     public static boolean isNull(Object object) {
         return object == null ? true : false;
     }
 
-    /**
-     * @Description: 判断字符串是否为空. null和纯空格均视为空
+    /**判断字符串是否为空. null和纯空格均视为空
      * @param str 字符串
-     * @return: true/false
+     * @return 字符串为null或纯空格 返回true, 否则返回false
      * @since 1.0
-     * @author: zongf
-     * @time: 2019-06-12 10:43:48
+     * @author zongf
+     * @created 2019-07-01
      */
     public static boolean isEmpty(String str) {
         if(str == null || "".equals(str.trim())){
@@ -38,13 +35,12 @@ public class ValidateUtil {
         return false;
     }
 
-    /**
-     * @Description: 判断集合是否为空. null和size为0均视为空
+    /**判断集合是否为空. null和size为0均视为空
      * @param collection 集合或其子类型, 如:collection, list, set等
-     * @return: true/false
+     * @return 集合为null或集合元素个数为0 返回true, 否则返回false
      * @since 1.0
-     * @author: zongf
-     * @time: 2019-06-12 10:45:42
+     * @author zongf
+     * @created 2019-07-01
      */
     public static boolean isEmpty(Collection collection) {
         if (collection == null || collection.size() == 0) {
@@ -53,13 +49,12 @@ public class ValidateUtil {
         return false;
     }
 
-    /**
-     * @Description: 判断map是否为空. null和size为0均视为空
-     * @param map map 类型
-     * @return: true/false
+    /**判断map是否为空. null和size为0均视为空
+     * @param map 类型
+     * @return map为null或map的size为0 返回true, 否则返回false.
      * @since 1.0
-     * @author: zongf
-     * @time: 2019-06-12 10:45:42
+     * @author zongf
+     * @created 2019-07-01
      */
     public static boolean isEmpty(Map map) {
         if (map == null || map.size() == 0) {
@@ -68,13 +63,12 @@ public class ValidateUtil {
         return false;
     }
 
-    /**
-     * @Description: 判断数组是否为空. null和length为0均视为空
+    /**判断数组是否为空. null和length为0均视为空
      * @param array 任意类型数组
-     * @return: true/false
+     * @return 对象为null或者长度为0返回true, 否则返回false.
      * @since 1.0
-     * @author: zongf
-     * @time: 2019-06-12 10:45:42
+     * @author zongf
+     * @created 2019-07-01
      */
     public static boolean isEmpty(Object[] array) {
         if (array == null || array.length == 0) {
@@ -85,61 +79,56 @@ public class ValidateUtil {
 
     //****************************************** 反义校验 **********************************************************
 
-    /**
-     * @Description: 判断对象是否非null
+    /**判断对象不为null
      * @param object 任意类型对象
-     * @return: true/false
+     * @return 对象为null 返回false, 否则返回true
      * @since 1.0
-     * @author: zongf
-     * @time: 2019-06-12 10:42:05
+     * @author zongf
+     * @created 2019-07-01
      */
     public static boolean isNotNull(Object object) {
         return !isNotNull(object);
     }
 
-    /**
-     * @Description: 判断字符串是否非空. null和纯空格均视为空
+    /**判断字符串是否非空. null和纯空格均视为空
      * @param str 字符串
-     * @return: true/false
+     * @return 字符串为null或为纯空格返回fasle, 否则返回true
      * @since 1.0
-     * @author: zongf
-     * @time: 2019-06-12 10:43:48
+     * @author zongf
+     * @created 2019-07-01
      */
     public static boolean isNotEmpty(String str) {
         return !isEmpty(str);
     }
 
-    /**
-     * @Description: 判断集合是否非空. null和size为0均视为空
+    /**判断集合是否非空. null和size为0均视为空
      * @param collection 集合或其子类型, 如:collection, list, set等
-     * @return: true/false
+     * @return 集合为null或集合的size为0返回false, 否则返回true
      * @since 1.0
-     * @author: zongf
-     * @time: 2019-06-12 10:45:42
+     * @author zongf
+     * @created 2019-07-01
      */
     public static boolean isNotEmpty(Collection collection) {
         return !isEmpty(collection);
     }
 
-    /**
-     * @Description: 判断map是否非空. null和size为0均视为空
-     * @param map map 类型
-     * @return: true/false
+    /**判断map是否非空. null和size为0均视为空
+     * @param map 类型
+     * @return map为null或map的size为0返回false, 否则返回true
      * @since 1.0
-     * @author: zongf
-     * @time: 2019-06-12 10:45:42
+     * @author zongf
+     * @created 2019-07-01
      */
     public static boolean isNotEmpty(Map map) {
         return !isEmpty(map);
     }
 
-    /**
-     * @Description: 判断数组是否非空. null和length为0均视为空
+    /**判断数组是否非空. null和length为0均视为空
      * @param array 任意类型数组
-     * @return: true/false
+     * @return 数组为null或数组长度为0返回false, 否则返回true
      * @since 1.0
-     * @author: zongf
-     * @time: 2019-06-12 10:45:42
+     * @author zongf
+     * @created 2019-07-01
      */
     public static boolean isNotEmpty(Object[] array) {
         return !isEmpty(array);
