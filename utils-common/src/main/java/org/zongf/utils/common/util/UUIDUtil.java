43ef1624410751146948d0d4340c44fa62dbbc9b
zongf0504 <15737250504@139.com>
2019-07-01 10:48:23 +0800

    修改注释格式

--- utils-common/src/main/java/org/zongf/utils/common/util/UUIDUtil.java
+++ utils-common/src/main/java/org/zongf/utils/common/util/UUIDUtil.java
@@ -4,43 +4,39 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-/**
- * @Description: 获取UUID 字符串
+/** UUID字符串工具类
  * @since 1.0
- * @author: zongf
- * @date: 2019-06-12 11:02
+ * @author zongf
+ * @created 2019-07-01
  */
 public class UUIDUtil {
 
-    /**
-     * @Description: 获取随机的UUID字符串, 32位小写字母与数字
-     * @return: String
+    /** 获取随机的UUID字符串, 32位小写字母与数字
+     * @return 32位小写字母与数字组成的字符串, 不包含连接符-
      * @since 1.0
-     * @author: zongf
-     * @time: 2019-06-12 11:14:30
+     * @author zongf
+     * @created 2019-07-01
      */
     public static String getLowerString(){
         return UUID.randomUUID().toString().replace("-", "").toLowerCase();
     }
 
-    /**
-     * @Description: 获取随机的UUID字符串, 32位大写字母与数字
-     * @return: String
+    /**获取随机的UUID字符串, 32位大写字母与数字
+     * @return 32位大写字母与数字组成的字符串, 不包含连接符-
      * @since 1.0
-     * @author: zongf
-     * @time: 2019-06-12 11:14:30
+     * @author zongf
+     * @created 2019-07-01
      */
     public static String getUpperString() {
         return UUID.randomUUID().toString().replace("-", "").toUpperCase();
     }
 
-    /**
-     * @Description: 批量获取随机UUID字符串, 32位小写字母与数字
-     * @param num 数量
-     * @return: List
+    /** 批量获取随机UUID字符串, 32位小写字母与数字
+     * @param num UUID字符串数量
+     * @return List UUID 字符串列表
      * @since 1.0
-     * @author: zongf
-     * @time: 2019-06-12 11:14:30
+     * @author zongf
+     * @created 2019-07-01
      */
     public static List<String> getLowerStrings(int num) {
         List<String> list = new ArrayList<>();
@@ -50,13 +46,12 @@ public class UUIDUtil {
         return list;
     }
 
-    /**
-     * @Description: 批量获取随机UUID字符串, 32位大写字母与数字
-     * @param num 数量
-     * @return: List
+    /**批量获取随机UUID字符串, 32位大写字母与数字
+     * @param num UUID字符串数量
+     * @return List UUID 字符串列表
      * @since 1.0
-     * @author: zongf
-     * @time: 2019-06-12 11:14:30
+     * @author zongf
+     * @created 2019-07-01
      */
     public static List<String> getUpperStrings(int num) {
         List<String> list = new ArrayList<>();
